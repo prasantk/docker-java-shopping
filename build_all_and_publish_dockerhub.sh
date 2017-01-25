a4ffc6738ad7ddffa5b64701c87610d38c37e4c0
Prasant Kumar <publicapi@live.com>
2017-01-25 17:06:14 +0530

    updated docker repository names

--- build_all_and_publish_dockerhub.sh
+++ build_all_and_publish_dockerhub.sh
@@ -2,21 +2,21 @@
 
 cd shopfront
 mvn clean install
-if docker build -t danielbryantuk/djshopfront . ; then
-  docker push danielbryantuk/djshopfront
+if docker build -t prasantk/djshopfront . ; then
+  docker push prasantk/djshopfront
 fi
 cd ..
 
 cd productcatalogue
 mvn clean install
-if docker build -t danielbryantuk/djproductcatalogue . ; then
-  docker push danielbryantuk/djproductcatalogue
+if docker build -t prasantk/djproductcatalogue . ; then
+  docker push prasantk/djproductcatalogue
 fi
 cd ..
 
 cd stockmanager
 mvn clean install
-if docker build -t danielbryantuk/djstockmanager . ; then
-  docker push danielbryantuk/djstockmanager
+if docker build -t prasantk/djstockmanager . ; then
+  docker push prasantk/djstockmanager
 fi
 cd ..
